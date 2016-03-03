a51df4de1556040462918e2778be1b870e388033
brady91700 <abderemane_brady@live.fr>
2016-03-03 13:15:52 +0100

    ajout modele_index.java

---
+++ Agence_immobiliere/src/modele/modele_index.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class modele_index {
+	private boolean page;
+		public boolean isPage() {
+		return page;
+	}
+	public void setPage(boolean page) {
+		this.page = page;
+	}
+		public modele_index(boolean page){
+			this.page=page;
+		}
+}
